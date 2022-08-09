59c9d8cd2dac3e8cb140106e66a366ea1045732a
Laura Casteran <laurac644@gmail.com>
2022-08-09 13:47:19 +0200

    Ajout images

--- example/src/App.js
+++ example/src/App.js
@@ -3,11 +3,13 @@ import React from 'react'
 import { Table } from '@laura/table'
 import '@laura/table/dist/index.css'
 
-import { contentStore } from './store/LocalStore'
-
 
 const App = () => {
-  return <Table data={[]}/>
+  const dataToDisplay = [{"Col1":"Value11", "Col2":"Value12", "Col3":"Value13"}, 
+  {"Col1":"Value21", "Col2":"Value22", "Col3":"Value23"}, 
+  {"Col1":"Value31", "Col2":"Value32", "Col3":"Value33"}, 
+  {"Col1":"Value41", "Col2":"Value42", "Col3":"Value43"}, ]
+  return <Table data={dataToDisplay}/>
 }
 
 export default App
